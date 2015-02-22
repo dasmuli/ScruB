71ac07fd0a30f969dca7764abfcde92e23e912c0
dasmuli <dasmuli@web.de>
2015-02-22 17:13:41 +0100

    Scrum data integrity check added, prio list length method added.

--- public/UnitTests/ScruBUnitTests.js
+++ public/UnitTests/ScruBUnitTests.js
@@ -1,4 +1,35 @@
 
-QUnit.test( "hello test", function( assert ) {
+QUnit.test( "qunit test", function( assert ) {
 	assert.ok( 1 == "1", "Passed!" );
+});
+
+QUnit.test( "scrum data test array exists", function( assert ) {
+	assert.notEqual( scrumDataArray, null );
+});
+
+QUnit.test( "scrum data init testdata", function( assert ) {
+	scrumDataManager.InitTestData();
+	assert.ok( scrumDataArray.length == 3 );
+});
+
+QUnit.test( "scrum data check integrity", function( assert ) {
+	scrumDataManager.InitTestData();
+	assert.equal( scrumDataManager.IsIntegrityOk(), true );
+	assert.equal( scrumDataManager.PriorityListLength(), 3 );
+	scrumDataArray[ 0 ].previousPriorityId = 1;
+	assert.equal( scrumDataManager.IsIntegrityOk(), false );
+	scrumDataArray[ 0 ].previousPriorityId = -1;
+	assert.equal( scrumDataManager.IsIntegrityOk(), true );
+	scrumDataArray[ 2 ].nextPriorityId = 1;
+	assert.equal( scrumDataManager.IsIntegrityOk(), false );
+	scrumDataArray[ 2 ].nextPriorityId = -1;
+});
+
+QUnit.test( "scrum data check prio list length", function( assert ) {
+	scrumDataManager.InitTestData();
+	assert.equal( scrumDataManager.PriorityListLength(), 3 );
+	scrumDataArray[ 1 ].nextPriorityId = -1;
+	assert.equal( scrumDataManager.PriorityListLength(), 2 );
+	scrumDataArray[ 0 ].nextPriorityId = -1;
+	assert.equal( scrumDataManager.PriorityListLength(), 1 );
 });
--- public/scrumdata.js
+++ public/scrumdata.js
@@ -8,8 +8,83 @@ var ScrumData = {  // Default value of properties
   complexity:			8,
   priority:				1,
   previousPriorityId:	-1,
+  nextPriorityId:		-1,
   printData : function(){  // Method which will display type of Animal
     console.log( 'scrumdata ' + this.id + " " + this.featurename + ' ' + complextiy );
   }
 }
 
+var scrumDataManager = {
+	priorityStartId: 0,
+	DataObject: function ( id ) {
+		this.id 				= id;
+		this.featurename 		= "TestData" + id;
+		this.complexity 		= 2;
+		this.previousPriorityId = -1;
+		this.nextPriorityId		= -1;
+	},
+    InitTestData: function () {
+		console.log( "Init test data called." );
+		scrumDataArray = new Array();
+		for( i = 0; i < 3; i++ )
+		{
+			scrumDataArray[ i ] = new scrumDataManager.DataObject( i );
+			if( i < 2 )
+			{
+				scrumDataArray[ i ].nextPriorityId = i + 1;
+			}
+			if( i > 0 )
+			{
+				scrumDataArray[ i ].previousPriorityId = i - 1;
+			}
+		}
+    },
+	PriorityListLength: function () {
+		var currentData = scrumDataArray[ scrumDataManager.priorityStartId ];
+		var count = 0;
+		console.log( "currentData id: " + currentData.id + ", next: " + currentData.nextPriorityId );
+		while( currentData.nextPriorityId != -1 && count < scrumDataArray.length )
+		{
+			console.log( "scrumDataManager.PriorityListLength: next" );
+			currentData = scrumDataArray[ currentData.nextPriorityId ];
+			count++;
+		}
+		return count + 1;
+    },
+	IsIntegrityOk: function () {
+		// check ids are existing exactly once
+		for( i = 0; i < scrumDataArray.length; i++ )
+		{
+			if( scrumDataArray[ i ].id != i )
+			{
+				return false;
+			}
+		}
+		// check priority list is ok
+		var currentData = scrumDataArray[ scrumDataManager.priorityStartId ];
+		if( currentData.previousPriorityId != -1 )
+		{
+			console.log( "scrumDataManager.IsIntegrityOk: Initial priority field " + currentData.id + " has previous element" );
+			return false;
+		}
+		var count = 0;
+		while( currentData.nextPriorityId != -1 && count < scrumDataArray.length )
+		{
+			nextData = scrumDataArray[ currentData.nextPriorityId ];
+			if( nextData.previousPriorityId != currentData.id )
+			{
+				console.log( "scrumDataManager.IsIntegrityOk: Linked list error: " + nextData.id + " has not set "
+					+ currentData.id + " as previous element, but is the successor.");
+				return false;
+			}
+			currentData = nextData;
+			count++;
+		}
+		if( count >= scrumDataArray.length )
+		{
+			console.log( "scrumDataManager.IsIntegrityOk: Too many elements in priority list" );
+			return false;
+		}
+		return true;
+	}
+}
