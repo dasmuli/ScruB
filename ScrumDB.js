0f6e2f67a1f9913d38bf27895eb5a5cfcceb526a
Dirk Wilking <dirk.wilking@email.de>
2015-02-28 22:38:36 +0100

    Adding of properties abandoned, usableloading result when loading non existing files added.

--- ScrumDB.js
+++ ScrumDB.js
@@ -3,12 +3,19 @@ var fs = require( 'fs' );
 
 this.scrumDataManagerList = new Array();
 
-this.scrumDataArray;
-this.parsedObject = {};
-
 this.LoadScrumDataSync = function( name )
 {
-    var preparsedData = fs.readFileSync( "data/" + name + ".json", 'utf8'); 
+    try
+    {
+         var preparsedData = fs.readFileSync( "data/" + name + ".json", 'utf8' ); 
+    } catch( e )
+    {
+     	 var data= {};
+	 data.priorityStartId = -1;
+	 data.lastFinishedId = -1;
+	 data.scrumDataArray = new Array();
+	 return data;
+    }
     if( preparsedData.length == 0 )
     {
         console.log( "ScrumDB::LoadScrumDataSync: Could not open file." );
@@ -18,20 +25,7 @@ this.LoadScrumDataSync = function( name )
 	data.scrumDataArray = new Array();
 	return data;
     }
-    console.log( "Loaded data:" + preparsedData );
-    this.parsedObject = JSON.parse( preparsedData );
-    this.CheckForMissingAttributes(); 
-    console.log( "Returing " + JSON.stringify( this.parsedObject, null, ' ' ) );
-    return this.parsedObject;
-}
-
-this.CheckForMissingAttributes = function()
-{
-   if( !( 'priorityStartId' in this.parsedObject ) )
-   {
-	   console.log( "adding prop" );
-       this.parsedObject[ 'priorityStartId' ] = -1;
-   }
+    return JSON.parse( preparsedData );
 }
 
 this.SaveScrumDataAsync = function( name, scrumDataArray, priorityStartId, callback )
--- test/testScrumDB.js
+++ test/testScrumDB.js
@@ -56,39 +56,17 @@ it('should receive a scrum data manager an ask if it is changed', function (done
 	  scrumDB.AddDataManager( scrumDataManager );
 });
 
-it('should automatically add missing elements while loading', function () {
+it('should return something legal when loading non existing files', function () {
 	  scrumDataArray = null;
 	  assert.equal( scrumDataArray, null );
-	  // create empty array
-	  var testArray = new Array();
-	  var emptyObject = {};
-	  testArray.push( emptyObject );
-	  testArray.push( emptyObject );
-	  var data = {
-            scrumDataArray: testArray
-	  };
-	  // and save it
-          fs.writeFileSync( "data/TestEmptyArray.json",
-			JSON.stringify( data, null, ' ' ) ); 
-	  data = scrumDB.LoadScrumDataSync( "TestEmptyArray" );
-	  console.log( "Obj:" + JSON.stringify( data, null, ' ' ) );
-	  assert.equal( data['priorityStartId'], -1 );
+	  assert.notEqual( scrumDataManager, null );
+	  scrumDataManager.priorityStartId = -1;
+	  var data = scrumDB.LoadScrumDataSync( "NonExisting" );
+	  assert.equal( data.scrumDataArray.length, 0 );
+	  assert.equal( data.priorityStartId, -1 );
 	  assert.equal( data.lastFinishedId, -1 );
-	  scrumDataArray = data.scrumDataArray;
-	  assert.equal( scrumDataArray[ 0 ].id, 0 );
-	  assert.equal( scrumDataArray[ 0 ].featurename, "Unknown" );
-	  assert.equal( scrumDataArray[ 0 ].complexity, 0 );
-	  assert.equal( scrumDataArray[ 0 ].isFinished, false );
-	  assert.equal( scrumDataArray[ 0 ].nextPriorityId, 1 );
-	  assert.equal( scrumDataArray[ 0 ].previousPriorityId, -1 );
-	  assert.equal( scrumDataArray[ 1 ].id, 1 );
-	  assert.equal( scrumDataArray[ 1 ].featurename, "Unknown" );
-	  assert.equal( scrumDataArray[ 1 ].complexity, 0 );
-	  assert.equal( scrumDataArray[ 1 ].isFinished, false );
-	  assert.equal( scrumDataArray[ 1 ].nextPriorityId, 1 );
-	  assert.equal( scrumDataArray[ 1 ].previousPriorityId, -1 );
-
 });
 
 
-} );
+
+} ); // of describe
